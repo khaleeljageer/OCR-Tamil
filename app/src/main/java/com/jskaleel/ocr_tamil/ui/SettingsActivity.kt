25ad190eb84413b5706fcfdc5f5188af29516af0
Khaleel Jageer <jskcse4@gmail.com>
2021-10-18 22:32:00 +0530

    settings page added

--- app/src/main/java/com/jskaleel/ocr_tamil/ui/SettingsActivity.kt
+++ app/src/main/java/com/jskaleel/ocr_tamil/ui/SettingsActivity.kt
@@ -2,12 +2,14 @@ package com.jskaleel.ocr_tamil.ui
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.HtmlCompat
 import com.jskaleel.ocr_tamil.BuildConfig
 import com.jskaleel.ocr_tamil.R
 import com.jskaleel.ocr_tamil.databinding.ActivitySettingsBinding
+import com.jskaleel.ocr_tamil.utils.Constants
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -32,8 +34,41 @@ class SettingsActivity : AppCompatActivity() {
             getString(R.string.vglug_foundation_desc),
             HtmlCompat.FROM_HTML_MODE_LEGACY
         )
+
+        binding.layoutKaniyam.setOnClickListener {
+            openUrl("http://www.kaniyam.com/")
+        }
+
+        binding.layoutVglug.setOnClickListener {
+            openUrl("https://vglug.org")
+        }
+
+        binding.rlSourceCodeLayout.setOnClickListener {
+            openUrl("https://github.com/khaleeljageer/OCR-Tamil")
+        }
+
+        binding.llShareApp.setOnClickListener {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    String.format(getString(R.string.share_text), Constants.STORE_URL)
+                )
+                type = "text/plain"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, getString(R.string.share_app))
+            startActivity(shareIntent)
+        }
     }
 
+    private fun openUrl(url: String) {
+        val shareIntent: Intent = Intent().apply {
+            action = Intent.ACTION_VIEW
+            data = Uri.parse(url)
+        }
+        startActivity(shareIntent)
+    }
 
     companion object {
         fun newIntent(context: Context): Intent {
--- app/src/main/java/com/jskaleel/ocr_tamil/utils/Constants.kt
+++ app/src/main/java/com/jskaleel/ocr_tamil/utils/Constants.kt
@@ -6,4 +6,5 @@ object Constants {
     const val TESS_DATA_NAME = "%s.traineddata"
     const val TESSERACT_DATA_DOWNLOAD_URL_BEST =
         "https://github.com/tesseract-ocr/tessdata_best/raw/4.0.0/%s.traineddata"
+    const val STORE_URL = "https://play.google.com/store/apps/details?id=com.jskaleel.ocr_tamil"
 }
