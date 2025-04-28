4fb9c758442b6f0c9386353629acdf3754749aab
Khaleel Jageer <jskcse4@gmail.com>
2025-04-28 15:02:33 +0530

    theme color changed

--- app/src/main/java/com/jskaleel/vizhi_tamil/ui/theme/Color.kt
+++ app/src/main/java/com/jskaleel/vizhi_tamil/ui/theme/Color.kt
@@ -2,10 +2,17 @@ package com.jskaleel.vizhi_tamil.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Light Theme Colors
+val Saffron40 = Color(0xFFFF6F00) // Primary: Saffron Orange
+val Teal40 = Color(0xFF4A6360) // Secondary: Muted Teal
+val Coral40 = Color(0xFFD81B60) // Tertiary: Soft Coral
+val OffWhite = Color(0xFFFFF8E1) // Background
+val LightCream = Color(0xFFFFFBFE) // Surface
+val SaffronContainer = Color(0xFFFFB300) // PrimaryContainer
+val TealContainer = Color(0xFFB2CBC8) // SecondaryContainer
+val CoralContainer = Color(0xFFFF8A80) // TertiaryContainer
+val ErrorRed = Color(0xFFB00020) // Error
+val LightRed = Color(0xFFFCD8D9) // ErrorContainer
+val DarkGray = Color(0xFF1C1B1F) // OnBackground, OnSurface
+val LightGray = Color(0xFFE0E3E3) // SurfaceVariant
+val MediumGray = Color(0xFF444746) // OnSurfaceVariant
