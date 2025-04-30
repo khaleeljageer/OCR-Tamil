bfcf0160af23b29a9efbb9870c6bc0c365563322
Khaleel Jageer <jskcse4@gmail.com>
2025-04-30 18:06:02 +0530

    fab text change

--- app/src/main/java/com/jskaleel/vizhi_tamil/ui/screens/home/HomeScreens.kt
+++ app/src/main/java/com/jskaleel/vizhi_tamil/ui/screens/home/HomeScreens.kt
@@ -3,8 +3,6 @@ package com.jskaleel.vizhi_tamil.ui.screens.home
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -12,7 +10,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.jskaleel.vizhi_tamil.R
 import com.jskaleel.vizhi_tamil.core.utils.CallBack
 
 @Composable
@@ -24,10 +24,17 @@ fun HomeScreen(
     ) {
 
         ExtendedFloatingActionButton(
-            modifier = Modifier.align(Alignment.BottomEnd).padding(16.dp),
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp),
             onClick = onNewClick,
-            icon = { Icon(Icons.Rounded.Add, "New") },
-            text = { Text(text = "New") },
+            icon = {
+                Icon(
+                    painter = painterResource(R.drawable.rounded_scanner_24),
+                    contentDescription = "Scan"
+                )
+            },
+            text = { Text(text = "Scan") },
             containerColor = MaterialTheme.colorScheme.primary,
         )
     }
